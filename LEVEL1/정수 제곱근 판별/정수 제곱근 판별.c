3d97619f1ec19a7d3a0f8c0fe9f900e69c2de1ab
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:49:54 +0900

    LEVEL1 정수 제곱근 판별 해결

---
+++ LEVEL1/정수 제곱근 판별/정수 제곱근 판별.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+long long solution(long long n) {
+    if ((int)sqrt(n) - sqrt(n) == 0) {
+        return pow(sqrt(n) + 1, 2);
+    }
+
+    return -1;
+}
+
+int main() {
+    solution(121);
+}
