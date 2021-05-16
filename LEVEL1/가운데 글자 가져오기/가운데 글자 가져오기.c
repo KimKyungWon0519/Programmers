17fb837502d7a5bf11179401d59ca23d2581fd55
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:38:07 +0900

    LEVEL1 가운데 글자 가져오기 해결

---
+++ LEVEL1/가운데 글자 가져오기/가운데 글자 가져오기.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* s) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer;
+    int length = strlen(s);
+    int index = 0;
+
+    if (length % 2 == 0) {
+        answer = (char*)calloc(3, sizeof(char));
+        answer[index++] = s[(length / 2) - 1];
+        answer[index++] = s[(length / 2)];
+    }
+    else {
+        answer = (char*)calloc(2, sizeof(char));
+        answer[index++] = s[(length / 2)];
+    }
+
+    return answer;
+}
+
+int main() {
+    solution("abcde");
+}
