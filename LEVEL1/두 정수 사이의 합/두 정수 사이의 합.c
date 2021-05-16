3c6f3086b99bfce2b802c6118666af089efb26a2
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:39:09 +0900

    LEVEL1 두 정수 사이의 합 해결

---
+++ LEVEL1/두 정수 사이의 합/두 정수 사이의 합.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int max = a, min = b;
+
+    if (a < b) {
+        max = b;
+        min = a;
+    }
+
+    for (int i = min; i <= max; i++) {
+        answer += i;
+    }
+
+    return answer;
+}
+
+int main() {
+    solution(3, 5);
+}
