ecf3006aaa42917e29e046476e64c2e972e7fc40
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:34:24 +0900

    LEVEL1 약수와 개수와 덧셈

---
+++ LEVEL1/약수의 개수와 덧셈/약수의 개수와 덧셈.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int left, int right) {
+    int answer = 0;
+    int count = 0;
+
+    for (int i = left; i <= right; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                count++;
+            }
+
+        }
+
+        answer += (count & 1) ? -i : i;
+        count = 0;
+    }
+
+
+    return answer;
+}
+
+int main() {
+    solution(13, 17);
+}
