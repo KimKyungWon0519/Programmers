7c2c691a049aada327898304815264d9aa310894
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:47:37 +0900

    LEVEL1 자릿수 더하기 해결

---
+++ LEVEL1/자릿수 더하기/자릿수 더하기.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int solution(int n) {
+    char* strNumber = (char*)calloc(6, sizeof(char));
+    int answer = 0;
+
+    sprintf(strNumber, "%d", n);
+
+    for (int i = 0; i < strlen(strNumber); i++) {
+        answer += strNumber[i] - '0';
+    }
+
+
+    return answer;
+}
+
+int main() {
+    solution(123);
+}
