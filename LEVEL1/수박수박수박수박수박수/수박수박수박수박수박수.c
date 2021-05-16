9c68379d6cbe58a1ee7687159f3e46212f471dc3
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:43:42 +0900

    LEVEL1 수박수박수박수박수박수? 해결

---
+++ LEVEL1/수박수박수박수박수박수/수박수박수박수박수박수.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(int n) {
+	// 리턴할 값은 메모리를 동적 할당해주세요.
+	char* answer = (char*)calloc((n * 3) + 1, sizeof(char));
+
+	for (int i = 0; i < n; i++) {
+		strcat(answer, i & 1 ? "박" : "수");
+	}
+
+	return answer;
+}
+
+int main() {
+	solution(3);
+}
