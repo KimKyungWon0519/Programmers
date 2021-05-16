79f3e1fb56f166564124cfc5fa77fc027c252e5c
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:48:38 +0900

    LEVEL1 자연수 뒤집어 배열로 만들기 해결

---
+++ LEVEL1/자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int* solution(long long n) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    int* answer;
+    char* strNumber = (char*)calloc(12, sizeof(char));
+
+    sprintf(strNumber, "%lld", n);
+
+    answer = (int*)calloc(strlen(strNumber), sizeof(int));
+
+    for (int i = 0; i < strlen(strNumber); i++) {
+        answer[i] = strNumber[strlen(strNumber) - i - 1] - '0';
+    }
+
+    return answer;
+}
+
+int main() {
+    solution(12345);
+}
