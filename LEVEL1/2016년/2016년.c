174c8459c7a5202e604190b081dee817200fc553
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:36:58 +0900

    LEVEL1 2016년 해결

---
+++ LEVEL1/2016년/2016년.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* week[7] = { "SAT", "SUN", "MON", "TUE", "WED", "THU", "FRI" };
+
+char* solution(int a, int b) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    char* answer = NULL;
+
+    int q = b, m = a, y = 2016;
+
+    if (m <= 2) {
+        m += 12;
+        y--;
+    }
+
+    int h = (q + 13 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7;
+
+    answer = week[h];
+    return answer;
+}
+
+int main() {
+    solution(5, 24);
+}
