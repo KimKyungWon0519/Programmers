ee7d0bac5c8483caba150b5d75c9d9eab6657c52
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:52:08 +0900

    LEVEL1 콜라츠 추측 해결

---
+++ LEVEL1/콜라츠 추축/콜라츠 추축.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(long num) {
+    printf("%d", sizeof(long));
+    int answer = 0;
+    int count = 0, i = 0;
+
+    while (i++ < 500) {
+        if (num == 1) {
+            return count;
+        }
+
+        if (num % 2 == 0) {
+            num /= 2;
+        }
+        else {
+            num = (num * 3) + 1;
+        }
+
+        count++;
+    }
+    return -1;
+}
+
+int main() {
+    solution(6);
+}
