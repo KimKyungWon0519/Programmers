3aac3c4b456c510080bd8e4eb055e7d476a2529c
KimKyungWon0519 <gwon080870@gmail.com>
2021-05-16 17:57:02 +0900

    LEVEL1 하샤드 수 해결

---
+++ LEVEL1/하샤드 수/하샤드 수.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool solution(int x) {
+    bool answer = true;
+    char strNumber[6];
+    int number = 0;
+
+    sprintf(strNumber, "%d", x);
+
+    for (int i = 0; i < strlen(strNumber); i++) {
+        number += (strNumber[i] - '0');
+    }
+
+    answer = x % number == 0 ? true : false;
+
+    return answer;
+}
+
+int main() {
+    solution(10);
+}
